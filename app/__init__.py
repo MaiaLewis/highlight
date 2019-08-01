1f2d9e7ab077ba90661401c3ddeef9cfef771edf
Maia <maia.j.lewis@gmail.com>
2019-08-01 11:18:59 -0500

    simpler test of large app restructure, works locally

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,7 @@ app.register_blueprint(auth_module)
 def index():
     print("got this far")
     return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
