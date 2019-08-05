220ec8f58d95c03d908baa3f360bd21feec73c1f
Maia <maia.j.lewis@gmail.com>
2019-08-05 08:58:48 -0500

    forgot to add search blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import flask
 from app.mod_auth.auth import mod_auth as auth_module
 from app.mod_save.save import mod_save as save_module
+from app.mod_search.search import mod_search as search_module
 from flask_cors import CORS
 
 app = flask.Flask(__name__, static_folder="../build/static",
@@ -10,6 +11,7 @@ CORS(app)
 app.secret_key = "secret"
 app.register_blueprint(auth_module)
 app.register_blueprint(save_module)
+app.register_blueprint(search_module)
 
 
 @app.route('/')
